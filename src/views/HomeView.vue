fcb48357ad7ba94473f90d31727fff535ac4784e
Branimir026 <br.belic@gmail.com>
2023-01-21 22:09:11 +0100

    simple home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>This is home page</h1>
+  </div>
 </template>
+
+<script>
+</script>
