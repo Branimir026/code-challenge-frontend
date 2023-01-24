aa5af37231768716bb2d1f068bbe146a676b71dc
Branimir026 <br.belic@gmail.com>
2023-01-24 21:17:46 +0100

    changed indentation from tabs to spaces

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,23 +1,23 @@
 <template>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between px-2">
-		<RouterLink to="/" class="nav-link">Home</RouterLink>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav">
-				<li class="nav-item">
-					<RouterLink to="/encoder" class="nav-link">Encoder</RouterLink>
-				</li>
-				<li class="nav-item" v-if="!isLoggedIn">
-					<RouterLink to="/login" class="nav-link">Login</RouterLink>
-				</li>
-				<li class="nav-item" v-else>
-					<RouterLink @click.prevent="logoutUser" to="" class="nav-link">Logout</RouterLink>
-				</li>
-			</ul>
-		</div>
-	</nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between px-2">
+    <RouterLink to="/" class="nav-link">Home</RouterLink>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <RouterLink to="/encoder" class="nav-link">Encoder</RouterLink>
+        </li>
+        <li class="nav-item" v-if="!isLoggedIn">
+          <RouterLink to="/login" class="nav-link">Login</RouterLink>
+        </li>
+        <li class="nav-item" v-else>
+          <RouterLink @click.prevent="logoutUser" to="" class="nav-link">Logout</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -29,7 +29,7 @@ export default {
     return {}
   },
 
-	computed: {
+  computed: {
     ...mapGetters(['isLoggedIn'])
   },
 
@@ -45,7 +45,7 @@ export default {
 <style scoped>
 
 .navbar-collapse {
-	flex-grow: 0;
+  flex-grow: 0;
 }
 
 @media (min-width: 1024px) {
