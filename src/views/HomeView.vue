1260fde683cdf62a1219ea92b66aecf041660ec2
Branimir026 <br.belic@gmail.com>
2023-01-25 16:02:42 +0100

    homepage change

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="home">
-    <h1>This is home page</h1>
+    <h1 class="title">Coding test for <a href="https://www.digihey.com/" target="_blank">Digihey</a></h1>
   </div>
 </template>
 
 <script>
 </script>
+
+<style scoped>
+
+	.home{
+		width: 100%;
+		max-width: 600px;
+		margin: auto;
+		text-align: center;
+	}
+
+	.title{
+		width: auto;
+		margin: auto;
+	}
+
+	.title a {
+		text-decoration: none;
+	}
+
+</style>
